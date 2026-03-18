1998462911027cf31252a7677c591a6ef34b1cc6
Ambra <trank13f@gmail.com>
2026-03-18 17:26:42 +0700

    create Skill model

--- db/models.py
+++ db/models.py
@@ -5,3 +5,10 @@ class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+
