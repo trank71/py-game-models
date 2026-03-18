9279a57e20edc22e0bb826488f07fd64a8e85664
Ambra <trank13f@gmail.com>
2026-03-18 18:20:11 +0700

    make main func through open and read JSON file make it to dict and through for cicl and classes create ich uniqe player

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as players_file:
+        players = json.load(players_file)
+        for player in players:
+            race, _ = Race.objects.get_or_create(
+                        name=player["race"]["name"],
+                        defaults={
+                            "description": player["race"]["description"]
+                        }
+            )
+
+            for skill in player["race"]["skills"]:
+                Skill.objects.get_or_create(
+                    name=skill["name"],
+                    defaults={
+                        "bonus": skill["bonus"],
+                        "races": race
+                    }
+                )
+
+            guild = Guild.objects.get_or_create(
+                name=player["guild"]["name"],
+                defaults={
+                    "description": player["guild"]["description"]
+                }
+            )
+            Player.objects.get_or_create(
+                name=player["name"],
+                defaults={
+                    "email": player["email"],
+                    "bio": player["bio"],
+                    "race": race,
+                    "guild": guild
+                }
+            )
 
 
 if __name__ == "__main__":
